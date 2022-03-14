aaf1bf8155276dc3b6857bdeb8176fc98b6a9e5c
Ryan Smith <65239974+rs19955@users.noreply.github.com>
2022-03-14 12:52:16 +0000

    task list updates

--- tasklist_script.js
+++ tasklist_script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+window.addEventListener("load", () => {
+    const form = document.querySelector("#new-task-form");
+    const input = document.querySelector("#new-task-input");
+    const list_el = document.querySelector("#tasks");
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        console.log("Sub");
+    })
+})
+
